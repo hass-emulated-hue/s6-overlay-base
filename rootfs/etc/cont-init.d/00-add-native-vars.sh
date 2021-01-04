fad40a89cd1fa69d1cbe1b9bea8e1dbffde7ad96
Alex <alexyao2015@gmail.com>
2021-01-04 16:18:19 -0600

    Fix adding nonexisting variables

--- rootfs/etc/cont-init.d/00-add-native-vars.sh
+++ rootfs/etc/cont-init.d/00-add-native-vars.sh
@@ -7,7 +7,18 @@
 mkdir -p /var/run/s6/container_environment
 
 # Reads all variables
-echo ${DATA_DIR} > /var/run/s6/container_environment/DATA_DIR
-echo ${HASS_TOKEN} > /var/run/s6/container_environment/HASS_TOKEN
-echo ${HASS_URL} > /var/run/s6/container_environment/HASS_URL
-echo ${VERBOSE} > /var/run/s6/container_environment/VERBOSE
+if [ -z "${DATA_DIR}" ]; then
+  echo ${DATA_DIR} > /var/run/s6/container_environment/DATA_DIR
+fi
+
+if [ -z "${HASS_TOKEN}" ]; then
+  echo ${HASS_TOKEN} > /var/run/s6/container_environment/HASS_TOKEN
+fi
+
+if [ -z "${HASS_URL}" ]; then
+  echo ${HASS_URL} > /var/run/s6/container_environment/HASS_URL
+fi
+
+if [ -z "${VERBOSE}" ]; then
+  echo ${VERBOSE} > /var/run/s6/container_environment/VERBOSE
+fi
