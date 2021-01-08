d2c6619afd2a69a4860c28fc823546023631634d
Alex <alexyao2015@gmail.com>
2021-01-07 23:20:43 -0600

    Accept repo name as well

--- rootfs-dev/etc/cont-init.d/20-install.sh
+++ rootfs-dev/etc/cont-init.d/20-install.sh
@@ -12,15 +12,23 @@ else
     release_version=${TAG_COMMIT_OR_BRANCH:-master}
 fi
 
-if [[ $release_version == *":"* ]]; then
+colon_count=`tr -dc ',' <<<"$release_version" | awk '{ print length; }'`
+repo_name="core"
+
+if [[ "$colon_count" == 1 ]]; then
   IFS=':' read -r -a array <<< "$release_version"
   username=${array[0]}
   ref=${array[1]}
+elif [ "$colon_count" == 2 ]; then
+  IFS=':' read -r -a array <<< "$release_version"
+  username=${array[0]}
+  repo_name=${array[1]}
+  ref=${array[2]}
 else
   username="hass-emulated-hue"
   ref=$release_version
 fi
-full_url="https://github.com/${username}/core/archive/${ref}.tar.gz"
+full_url="https://github.com/${username}/${repo_name}/archive/${ref}.tar.gz"
 bashio::log.info "Installing Emulated Hue version '${release_version}' (${full_url})..."
 curl -Lo /tmp/emulator.tar.gz "${full_url}"
 mkdir -p /tmp/emulator
