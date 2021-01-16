3ea8db71211dfe8cdb210aa4e323fb4dc0caafff
Alex <alexyao2015@gmail.com>
2021-01-15 21:22:50 -0600

    Properly export variables

--- scripts/10-entrypoint.sh
+++ scripts/10-entrypoint.sh
@@ -8,19 +8,19 @@
 if bashio::fs.file_exists '/data/options.json'; then
 
   if bashio::config.has_value 'data'; then
-      echo DATA_DIR=$(bashio::config 'data')
+      export DATA_DIR=$(bashio::config 'data')
   fi
 
   if bashio::config.has_value 'token'; then
-      echo HASS_TOKEN=$(bashio::config 'token')
+      export HASS_TOKEN=$(bashio::config 'token')
   fi
 
   if bashio::config.has_value 'url'; then
-      echo HASS_URL=$(bashio::config 'url')
+      export HASS_URL=$(bashio::config 'url')
   fi
 
   if bashio::config.has_value 'verbose'; then
-      echo VERBOSE=$(bashio::config 'verbose')
+      export VERBOSE=$(bashio::config 'verbose')
   fi
 
 fi
